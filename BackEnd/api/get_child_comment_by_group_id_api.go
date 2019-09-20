85fdf4a42b15f30c7d7ea9a0861ed461f49047a0
honeyhm <haniemlk@gmail.com>
2019-09-20 11:24:00 -0700

    message related apis

---
+++ BackEnd/api/get_child_comment_by_group_id_api.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"HealthCampaign/BackEnd/model"
+	_"HealthCampaign/BackEnd/model"
+	_"HealthCampaign/BackEnd/usecase"
+	"github.com/kataras/golog"
+	"github.com/kataras/iris"
+)
+
+func AllMessage2(Ctx iris.Context){
+	id:=Ctx.URLParam("id")
+	p,gtAllErr:=MessageUsecase.GetAllByGroupId(id)
+	t:=make([]model.Message,0)
+	for i:=0;i<len(p) ;i++  {
+		if p[i].ParentId != ""{
+			t= append(t, p[i])
+		}
+	}
+	golog.Info(gtAllErr)
+	Ctx.JSON(&t)
+}
--- BackEnd/api/get_parent_comment_by_group_id_api.go
+++ BackEnd/api/get_parent_comment_by_group_id_api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"HealthCampaign/BackEnd/model"
 	_"HealthCampaign/BackEnd/model"
 	_"HealthCampaign/BackEnd/usecase"
 	"github.com/kataras/golog"
@@ -10,6 +11,12 @@ import (
 func AllMessage(Ctx iris.Context){
 	id:=Ctx.URLParam("id")
 	p,gtAllErr:=MessageUsecase.GetAllByGroupId(id)
+	t:=make([]model.Message,0)
+	for i:=0;i<len(p) ;i++  {
+		if p[i].ParentId == ""{
+			t= append(t, p[i])
+		}
+	}
 	golog.Info(gtAllErr)
-	Ctx.JSON(&p)
+	Ctx.JSON(&t)
 }
